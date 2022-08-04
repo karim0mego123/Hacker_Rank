7e62f11b9c7e6c289b614ba16289b88df6a4239f
karim_elmlegy <karim.ayman.kemo@gmail.com>
2022-08-04 04:28:28 +0200

    solfrequency-of-digits-1

---
+++ frequency-of-digits-1/main.c
@@ -0,0 +1,33 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 4, 2022
+ *      Author: karim
+ */
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	int  a[10] = {0} , i = 0 , r ;
+	char x[1000] ;
+	scanf("%s",x) ;
+	r = strlen(x) ;
+	while( r+1 )
+	{
+	    --r ;
+	    	if ( x[r] < '0' || x[r] >'9' )
+			continue ;
+		i= x[r] ;
+		++a[i-48] ;
+	}
+	for (  i = 0 ; i<10 ; i++ )
+	{
+		printf("%d ",a[i] ) ;
+	}
+	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+	return 0;
+}
+
+
