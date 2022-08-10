580f5e84856b9b326149a74d07751a080f9b8626
karim_elmlegy <karim.ayman.kemo@gmail.com>
2022-08-10 13:26:42 +0200

    sol_Students Marks Sum

---
+++ Students Marks Sum/main_1.c
@@ -0,0 +1,51 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 10, 2022
+ *      Author: karim
+ */
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int marks_summation ( int *marks , int number_of_students , char gender )
+{
+	int  i =0 ;
+	int sum1 = 0 , sum2 = 0 ;
+
+	for ( i = 0 ; i<number_of_students ; i = i+2 )
+	{
+		sum1 += marks[i] ;
+	}
+	for ( i = 1 ; i<number_of_students ; i = i+2 )
+	{
+		sum2 += marks[i] ;
+	}
+	if ( gender == 'b' )
+		return sum1 ;
+	else
+		return sum2 ;
+}
+
+
+
+int main() {
+	int number_of_students;
+	char gender;
+	int sum;
+
+	scanf("%d", &number_of_students);
+	int *marks = (int *) malloc(number_of_students * sizeof (int));
+
+	for (int student = 0; student < number_of_students; student++) {
+		scanf("%d", (marks + student));
+	}
+
+	scanf(" %c", &gender);
+	sum = marks_summation(marks, number_of_students, gender);
+	printf("%d", sum);
+	free(marks);
+
+	return 0;
+}
+
